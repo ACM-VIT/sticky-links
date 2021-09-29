9c33d56af9acd11cf7774a5c8b67846769fbd4bd
Samridh29 <samridhagarwal011@gmail.com>
2021-09-29 15:36:57 +0530

    Web View

--- Sticky Links/Controllers/WebViewController.swift
+++ Sticky Links/Controllers/WebViewController.swift
@@ -6,24 +6,32 @@
 //
 
 import UIKit
+import WebKit
 
 class WebViewController: UIViewController {
-
+ 
+    var webView: WKWebView!
+    var selectedLink : Items?
+    
+    override func viewWillAppear(_ animated: Bool) {
+        navigationItem.title = selectedLink?.title
+    }
+    override func loadView() {
+        webView = WKWebView()
+        webView.navigationDelegate = self
+        view = webView
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        let url = URL(string: (selectedLink?.link)!)!
+        webView.load(URLRequest(url: url))
+        webView.allowsBackForwardNavigationGestures = true
         // Do any additional setup after loading the view.
     }
     
+}
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+//MARK: WebView
+extension WebViewController:WKNavigationDelegate{
+    
 }
